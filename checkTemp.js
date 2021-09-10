65fe906a196a2c133d0e506ae85adf2c70370d0f
Kristofer Järv <90023688+KristoferJarv@users.noreply.github.com>
2021-09-10 11:08:01 +0300

    checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,4 +1,4 @@
-/*
+ /*
   Katlal on kolm temoperatuurivahemikku
   0-20 on liiga külm          -1
   21-40 on paras temperatuur   0 
@@ -6,15 +6,49 @@
   GitHubi: homework/checkTemp.js
 */
 function checkTemp (temp) {
-      console.log("temp on "+temp)
-      if (temp < 0) {
-          console.log("Liiga Külm")
-          return -1;
-      } else if (temp <=40) {
-          return 0;
-      } else {
-          return 1;
-       
-      }
+    console.log("temp on "+temp)
+    if (temp < 20) {
+        console.log("Liiga Külm")
+        return -1;
+    } else if (temp <=40) {
+      console.log("paras temperatuur")
+        return 0;
 
-  }
+    } else {
+      console.log("liiga kuum")
+        return 1;
+     
+    }
+
+}
+
+
+let tulemus = checkTemp(5)
+
+checkTemp(5)
+checkTemp(15)
+checkTemp(30)
+checkTemp(45)
+checkTemp(55)
+
+
+function checkTemp() {
+let Temp = 5;
+temp = Math.random()*60 
+}
+
+
+function summa(a,b ) {
+    return a+b
+}
+let Vastus = summa(2,7)
+
+function kasKütta(temp) {
+return temp < 20
+}
+
+function kasJahutada(temp) {
+  returntemp > 40
+}
+
+let kasJahutada = true;
