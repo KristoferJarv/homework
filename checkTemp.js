5a9862b2681c7e4438cf2d4f5d55f29fb1190745
Kristofer Järv <90023688+KristoferJarv@users.noreply.github.com>
2021-09-24 12:14:39 +0300

    checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -5,50 +5,17 @@
   41-60 on liiga kuum          1 
   GitHubi: homework/checkTemp.js
 */
-function checkTemp (temp) {
-    console.log("temp on "+temp)
-    if (temp < 20) {
-        console.log("Liiga Külm")
-        return -1;
-    } else if (temp <=40) {
-      console.log("paras temperatuur")
-        return 0;
 
-    } else {
-      console.log("liiga kuum")
-        return 1;
-     
+function checkTemp(temp) {
+  console.log(temp);
+  if (temp <= 20) {
+  console.log("liiga külm")
+  return -1;
+  } else if (temp <= 40) {
+  console.log("paras temperatuur")
+  return 0;
+  } else {
+  console.log("liiga kuum")
+  return 1;
     }
-
-}
-
-
-let tulemus = checkTemp(5)
-
-checkTemp(5)
-checkTemp(15)
-checkTemp(30)
-checkTemp(45)
-checkTemp(55)
-
-
-function checkTemp() {
-let Temp = 5;
-temp = Math.random()*60 
-}
-
-
-function summa(a,b ) {
-    return a+b
-}
-let Vastus = summa(2,7)
-
-function kasKütta(temp) {
-return temp < 20
-}
-
-function kasJahutada(temp) {
-  returntemp > 40
-}
-
-let kasJahutada = true;
+  }
